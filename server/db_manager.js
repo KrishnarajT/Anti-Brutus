2969d045f08d077337357880fb2eb0c6b89fbe38
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-08-22 23:20:35 +0530

    db_manger.js insertUser function update (#14) (#15)
    Co-authored-by: Parth Zarekar <90327206+Parth4123@users.noreply.github.com>

--- server/db_manager.js
+++ server/db_manager.js
@@ -13,7 +13,7 @@ class DatabaseManager {
 	initializeDatabase() {
 		const createTableQuery = `
 			CREATE TABLE IF NOT EXISTS users (
-				id INTEGER PRIMARY KEY,
+				id INTEGER PRIMARY KEY ,
 				email TEXT NOT NULL,
 				password TEXT NOT NULL
 			)
@@ -27,7 +27,7 @@ class DatabaseManager {
 			}
 		});
 	}
-    test()  {
+    async test()  {
         const query = `
             SELECT * FROM users
         `;
@@ -41,9 +41,22 @@ class DatabaseManager {
         });
     }
 
-    
-
-
+	async insertUser(email ,password) {
+        const insertQuery = `
+            INSERT INTO users (email, password)
+            VALUES (?, ?)
+        `;
+	
+        this.db.run(insertQuery, [email,password], (err) => {
+            if (err) {
+                console.log("User not inserted.", err);
+				return false
+            } else {
+                console.log("User inserted successfully.");
+				return true;
+            }
+        });
+    }
 	// Other methods...
 }
 
