309a175d819ee282cecbab17e6f151f573763fcb
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-09-01 12:01:58 +0530

    Added images

--- src/pages/VaultContent.js
+++ src/pages/VaultContent.js
@@ -12,6 +12,7 @@ const VaultContent = () => {
 			url: "https://facebook.com",
 			description: "Your Favourtites",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.facebook.com/&size=128",
 		},
 		{
 			id: 2,
@@ -21,6 +22,17 @@ const VaultContent = () => {
 			url: "https://instagram.com",
 			description: "The place for your passwords",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.instagram.com/&size=128",
+		},
+		{
+			id: 3,
+			name: "Google",
+			username: "username",
+			password: "password",
+			url: "https://google.com",
+			description: "The place for your passwords",
+			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://google.com&size=128",
 		},
 		{
 			id: 3,
@@ -30,6 +42,57 @@ const VaultContent = () => {
 			url: "https://twitter.com",
 			description: "Safely Store your cards",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://twitter.com&size=128",
+		},
+		{
+			id: 3,
+			name: "Icones",
+			username: "username",
+			password: "password",
+			url: "https://icones.js.org/",
+			description: "Safely Store your cards",
+			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://icones.js.org&size=128",
+		},
+		{
+			id: 3,
+			name: "Whatsapp",
+			username: "username",
+			password: "password",
+			url: "https://web.whatsapp.com/",
+			description: "Safely Store your cards",
+			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://web.whatsapp.com/&size=128",
+		},
+		{
+			id: 3,
+			name: "Chat GPT",
+			username: "username",
+			password: "password",
+			url: "https://chat.openai.com/",
+			description: "Safely Store your cards",
+			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://chat.openai.com/&size=128",
+		},
+		{
+			id: 3,
+			name: "Amazon",
+			username: "username",
+			password: "password",
+			url: "https://www.amazon.in/",
+			description: "Safely Store your cards",
+			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.amazon.in/&size=128",
+		},
+		{
+			id: 3,
+			name: "Github",
+			username: "username",
+			password: "password",
+			url: "https://github.com",
+			description: "Safely Store your cards",
+			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://github.com&size=128",
 		},
 		{
 			id: 3,
@@ -39,6 +102,7 @@ const VaultContent = () => {
 			url: "https://twitter.com",
 			description: "Safely Store your cards",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://google.com&size=128",
 		},
 		{
 			id: 3,
@@ -48,6 +112,7 @@ const VaultContent = () => {
 			url: "https://twitter.com",
 			description: "Safely Store your cards",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://google.com&size=128",
 		},
 		{
 			id: 3,
@@ -57,6 +122,7 @@ const VaultContent = () => {
 			url: "https://twitter.com",
 			description: "Safely Store your cards",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://google.com&size=128",
 		},
 		{
 			id: 3,
@@ -66,6 +132,7 @@ const VaultContent = () => {
 			url: "https://twitter.com",
 			description: "Safely Store your cards",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://google.com&size=128",
 		},
 		{
 			id: 3,
@@ -75,6 +142,7 @@ const VaultContent = () => {
 			url: "https://twitter.com",
 			description: "Safely Store your cards",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://google.com&size=128",
 		},
 		{
 			id: 3,
@@ -84,6 +152,7 @@ const VaultContent = () => {
 			url: "https://twitter.com",
 			description: "Safely Store your cards",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://google.com&size=128",
 		},
 		{
 			id: 3,
@@ -93,6 +162,7 @@ const VaultContent = () => {
 			url: "https://twitter.com",
 			description: "Safely Store your cards",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://google.com&size=128",
 		},
 		{
 			id: 3,
@@ -102,6 +172,7 @@ const VaultContent = () => {
 			url: "https://twitter.com",
 			description: "Safely Store your cards",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://google.com&size=128",
 		},
 		{
 			id: 3,
@@ -111,6 +182,7 @@ const VaultContent = () => {
 			url: "https://twitter.com",
 			description: "Safely Store your cards",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://google.com&size=128",
 		},
 		{
 			id: 3,
@@ -120,6 +192,7 @@ const VaultContent = () => {
 			url: "https://twitter.com",
 			description: "Safely Store your cards",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://google.com&size=128",
 		},
 		{
 			id: 3,
@@ -129,6 +202,7 @@ const VaultContent = () => {
 			url: "https://twitter.com",
 			description: "Safely Store your cards",
 			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://google.com&size=128",
 		},
 		{
 			id: 3,
@@ -138,51 +212,7 @@ const VaultContent = () => {
 			url: "https://twitter.com",
 			description: "Safely Store your cards",
 			date: "12/12/12",
-		},
-		{
-			id: 3,
-			name: "Twitter",
-			username: "username",
-			password: "password",
-			url: "https://twitter.com",
-			description: "Safely Store your cards",
-			date: "12/12/12",
-		},
-		{
-			id: 3,
-			name: "Twitter",
-			username: "username",
-			password: "password",
-			url: "https://twitter.com",
-			description: "Safely Store your cards",
-			date: "12/12/12",
-		},
-		{
-			id: 3,
-			name: "Twitter",
-			username: "username",
-			password: "password",
-			url: "https://twitter.com",
-			description: "Safely Store your cards",
-			date: "12/12/12",
-		},
-		{
-			id: 3,
-			name: "Twitter",
-			username: "username",
-			password: "password",
-			url: "https://twitter.com",
-			description: "Safely Store your cards",
-			date: "12/12/12",
-		},
-		{
-			id: 3,
-			name: "Twitter",
-			username: "username",
-			password: "password",
-			url: "https://twitter.com",
-			description: "Safely Store your cards",
-			date: "12/12/12",
+			icon: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://google.com&size=128",
 		},
 	];
 	useEffect(() => {
@@ -191,7 +221,6 @@ const VaultContent = () => {
 		html.classList.add("overflow-hidden");
 
 		// get the passwords from the database
-
 	}, []);
 	const [selected_password, setSelectedPassword] = React.useState(null);
 	const [name, setName] = useState("");
@@ -212,12 +241,12 @@ const VaultContent = () => {
 				</div>
 				<div className="px-4 w-auto mr-10 text-2xl flex items-center gap-2 outline outline-1 rounded-full">
 					<div className="rounded-full h-14 w-14 p-2 flex items-center justify-center">
-						<IconSearch className="text-5xl text-primary-content h-7 w-7" />
+						<IconSearch className="text-5xl text-base-content h-7 w-7" />
 					</div>
 					<input
 						type="text"
 						placeholder="Search"
-						className="text-2xl h-14 w-full rounded-2xl border-none outline-none"
+						className="text-2xl h-14 w-full rounded-2xl border-none outline-none bg-transparent"
 					/>
 				</div>
 				<div className="flex items-center">
@@ -235,6 +264,7 @@ const VaultContent = () => {
 						{/* head */}
 						<thead>
 							<tr className="text-3xl text-base-content border-base-content outline rounded-ss-2xl rounded-se-2xl">
+								<th></th>
 								<th>Name</th>
 								<th>Username</th>
 								<th>Date</th>
@@ -244,17 +274,26 @@ const VaultContent = () => {
 							{passwords.map((password) => {
 								return (
 									<tr
-										className="h-14 transition-all duration-200 transform-gpu hover:bg-base-200 hover:cursor-pointer border-base-content"
+										className="h-16 transition-all duration-200 transform-gpu hover:bg-base-200 hover:cursor-pointer border-base-content"
 										onClick={() => {
 											setSelectedPassword(password);
-											// set values as well from this password. 
+											// set values as well from this password.
 											setName(password.name);
 											setUsername(password.username);
 											setPassword(password.password);
 											setUrl(password.url);
-											setDescription(password.description);
+											setDescription(
+												password.description
+											);
 										}}
 									>
+										<td>
+											<img
+												src={password.icon}
+												className="h-12 w-12 rounded-full outline outline-2 outline-offset-2 outline-primary"
+												alt=""
+											/>
+										</td>
 										<td className="hover:text-accent text-3xl transition-all duration-300">
 											{password.name}
 										</td>
@@ -275,13 +314,14 @@ const VaultContent = () => {
 						<div className="text-5xl font-bold text-center text-base-content">
 							Details
 						</div>
-						<button className="btn btn-primary btn-lg text-2xl" onClick={
-							() => {
+						<button
+							className="btn btn-primary btn-lg text-2xl"
+							onClick={() => {
 								// save the password
-								// create a json obejct containing the new values of the password. 
-								// send it to the server. 
-							}
-						}>
+								// create a json obejct containing the new values of the password.
+								// send it to the server.
+							}}
+						>
 							Save
 							<IconDeviceFloppy className="text-5xl text-primary-content h-8 w-8" />
 						</button>
@@ -290,9 +330,32 @@ const VaultContent = () => {
 					{/* textbox for name and username */}
 					<div className="form-control w-full max-w-xs">
 						<label className="label">
-							<span className="label-text text-xl">
-								Password Name
-							</span>
+							<span className="label-text text-xl">Name</span>
+						</label>
+						<input
+							type="text"
+							placeholder="Type here"
+							className="input input-bordered w-full max-w-xs"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</div>
+					<div className="form-control w-full max-w-xs">
+						<label className="label">
+							<span className="label-text text-xl">UserName</span>
+						</label>
+						<input
+							type="text"
+							placeholder="Type here"
+							className="input input-bordered w-full max-w-xs"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+					</div>
+					{/* textbox for password */}
+					<div className="form-control w-full max-w-xs">
+						<label className="label">
+							<span className="label-text text-xl">Password</span>
 						</label>
 						<input
 							type="text"
@@ -302,9 +365,20 @@ const VaultContent = () => {
 							onChange={(e) => setName(e.target.value)}
 						/>
 					</div>
-					{/* textbox for password */}
 
 					{/* textbox for url */}
+					<div className="form-control w-full max-w-xs">
+						<label className="label">
+							<span className="label-text text-xl">url</span>
+						</label>
+						<input
+							type="text"
+							placeholder="Type here"
+							className="input input-bordered w-full max-w-xs"
+							value={url}
+							onChange={(e) => setUrl(e.target.value)}
+						/>
+					</div>
 
 					{/* textbox for description */}
 					<textarea
