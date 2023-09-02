eb2c5a74fa24687572d3d0c7c6542afd244b7f73
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-09-02 13:11:34 +0530

    all vault functions work

--- src/pages/Vaults.js
+++ src/pages/Vaults.js
@@ -1,12 +1,18 @@
 import React, { useCallback } from "react";
 import { useEffect } from "react";
 import { ThemeContext } from "../context/ThemeContext";
-import { IconArrowRight, IconPlus } from "@tabler/icons-react";
+import {
+	IconArrowRight,
+	IconEraser,
+	IconPlus,
+	IconTrash,
+} from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
 import select_image from "../utils/images";
 import axios from "axios";
 import { BaseUrlContext } from "../context/BaseUrlContext";
 import { UserInfoContext } from "../context/UserInfoContext";
+import { CheckBadgeIcon } from "@heroicons/react/24/solid";
 
 const Vaults = () => {
 	const base_url = React.useContext(BaseUrlContext).baseUrl;
@@ -17,6 +23,7 @@ const Vaults = () => {
 	}, []);
 
 	const [vaults, setVaults] = React.useState([]);
+	const [deleteVaultID, setdeleteVaultID] = React.useState(null);
 	const [newVaultName, setNewVaultName] = React.useState("");
 	const [newVaultDescription, setNewVaultDescription] = React.useState("");
 
@@ -24,7 +31,6 @@ const Vaults = () => {
 	const navigate = useNavigate();
 
 	useEffect(() => {
-		console.log(theme);
 		if (theme === "light") {
 			const light_button = document.getElementById("light_button");
 			light_button.click();
@@ -35,7 +41,55 @@ const Vaults = () => {
 		if (vaults.length === 0) {
 			getVaults();
 		}
-	});
+	}, [vaults, theme]);
+
+	const handleDeleteVault = async () => {
+		console.log(deleteVaultID);
+		const response = await axios
+			.post(
+				`${base_url}/delete_vault`,
+				{},
+				{
+					params: {
+						vault_id: deleteVaultID,
+					},
+				}
+			)
+			.then((response) => {
+				return response;
+			})
+			.catch((error) => {
+				console.error(error);
+				alert("server not running! a simulated response is being sent");
+				const response = {
+					data: {
+						message: "simulation",
+					},
+				};
+				return response;
+			});
+		if (response.data.message === "simulation") {
+			alert("Vault Deleted");
+		} else if (response.data.message === "success") {
+			setVaults([]);
+			const toast = document.querySelector(".toast");
+			toast.classList.remove("hidden");
+			const toast_content = document.getElementById("toast_content");
+			toast_content.innerHTML = "Vault Deleted";
+			setTimeout(() => {
+				toast.classList.add("hidden");
+			}, 3000);
+		} else if (response.data.message === "failure") {
+			const toast = document.querySelector(".toast");
+			toast.classList.remove("hidden");
+			const toast_content = document.getElementById("toast_content");
+			toast_content.innerHTML = "Could not Delete Vault";
+			setTimeout(() => {
+				toast.classList.add("hidden");
+			}, 3000);
+		}
+		setdeleteVaultID(null);
+	};
 
 	const getVaults = async () => {
 		const response = await axios
@@ -81,15 +135,25 @@ const Vaults = () => {
 				},
 			]);
 		} else {
+			// set vaults but also set the image for each vault
+			response.data.data.forEach((vault) => {
+				vault.vault_image = select_image()["image"];
+			});
 			setVaults(response.data.data);
 			console.log(vaults);
 		}
 		console.log("password sending", response.data);
 	};
 
-	const handleSave = async () => {
+	const handleAddNewVaultSave = async () => {
 		const modal = document.getElementById("my_modal_3");
 		modal.close();
+
+		// check if the fields are set:
+		if (newVaultName === "" || newVaultDescription === "") {
+			alert("Please fill all the fields");
+			return;
+		}
 
 		// send request to server to add a new vault.
 		const response = await axios
@@ -117,7 +181,20 @@ const Vaults = () => {
 				};
 				return response;
 			});
-		console.log("password sending", response.data);
+		if (response.data.message === "simulation") {
+			alert("Vault Added");
+		} else if (response.data.message === "success") {
+			setVaults([]);
+		} else if (response.data.message === "failure") {
+			// toast
+			const toast = document.querySelector(".toast");
+			toast.classList.remove("hidden");
+			const toast_content = document.getElementById("toast_content");
+			toast_content.innerHTML = "Vault Already Exists";
+			setTimeout(() => {
+				toast.classList.add("hidden");
+			}, 3000);
+		}
 	};
 	return (
 		<div>
@@ -145,14 +222,12 @@ const Vaults = () => {
 			<div>
 				<div className="overflow-x-auto p-8 px-20 flex flex-wrap justify-center">
 					{vaults.length > 0
-						? (vaults.map((vault) => {
+						? vaults.map((vault) => {
 								return (
 									<div className="card w-1/4 h-72 bg-base-100 shadow-xl image-full m-4">
 										<div
 											style={{
-												backgroundImage: `url("${
-													select_image()["image"]
-												}")`,
+												backgroundImage: `url("${vault.vault_image}")`,
 												backgroundSize: "cover",
 												backgroundPosition: "center",
 												backgroundRepeat: "no-repeat",
@@ -160,9 +235,24 @@ const Vaults = () => {
 											className="image-full rounded-xl h-full w-full"
 										></div>
 										<div className="card-body">
-											<h2 className="card-title text-3xl">
+											<div className="card-title text-2xl flex justify-between">
 												{vault.vault_name}
-											</h2>
+												<div
+													className=" w-8 h-8 hover:scale-110 duration-200 transition-all transform-gpu"
+													onClick={() => {
+														setdeleteVaultID(
+															vault.vault_id
+														);
+														const modal =
+															document.getElementById(
+																"my_modal_1"
+															);
+														modal.showModal();
+													}}
+												>
+													<IconEraser className="h-8 w-8" />
+												</div>
+											</div>
 											<p className="text-2xl">
 												{vault.vault_description}
 											</p>
@@ -183,7 +273,7 @@ const Vaults = () => {
 										</div>
 									</div>
 								);
-						  }))
+						  })
 						: "No Vaults Found"}
 				</div>
 			</div>
@@ -224,9 +314,34 @@ const Vaults = () => {
 					</div>
 					<div className="modal-action">
 						{/* if there is a button in form, it will close the modal */}
-						<button className="btn" onClick={handleSave}>
+						<button className="btn" onClick={handleAddNewVaultSave}>
 							Save
 						</button>
+					</div>
+				</form>
+			</dialog>
+			<div className="toast toast-end duration-300 transform-gpu ease-in-out hidden">
+				<div className="alert alert-success bg-primary">
+					<span className="flex items-center gap-4 text-2xl">
+						<CheckBadgeIcon className="w-10 h-10" />
+						<p id="toast_content">Vault Added</p>
+					</span>
+				</div>
+			</div>
+			<dialog id="my_modal_1" className="modal">
+				<form method="dialog" className="modal-box">
+					<h3 className="font-bold text-lg">Hello!</h3>
+					<p className="py-4">
+						Are you sure you want to delete this vault? This will
+						delete all Passwords in the Vault, and it is
+						irreversable!
+					</p>
+					<div className="modal-action flex gap-4">
+						{/* if there is a button in form, it will close the modal */}
+						<button className="btn" onClick={handleDeleteVault}>
+							Yes, Delete!
+						</button>
+						<button className="btn">NO!</button>
 					</div>
 				</form>
 			</dialog>
