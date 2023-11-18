125f43a0334b63c68506a012d29d0754d7a25929
Saubhagya Singh <76896543+SaubhagyaSingh@users.noreply.github.com>
2023-11-18 12:37:41 +0530

    about page made responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -47,9 +47,9 @@ export function Navbar(props) {
   }
 
   return (
-    <div className="pr-4 mr-4 z-50">
+    <div className="pr-4 mr-2 ml-2 z-50 md:pr-4 mr-0 ml-0">
       <div
-        className={`navbar bg-primary rounded-xl text-primary-content flex-row-reverse justify-between m-4 
+        className={`navbar bg-primary rounded-xl text-primary-content flex-row-reverse justify-between m-2 
 			md:flex-row`}
       >
         {/* name */}
